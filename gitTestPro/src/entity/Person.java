ff1b23511f28b1ace0106deb5aced2ebaf9549d5
wjx <wjx1194953703@163.com>
2020-05-22 22:16:51 +0800

    添加相关的geter和setter方法

--- gitTestPro/src/entity/Person.java
+++ gitTestPro/src/entity/Person.java
@@ -5,4 +5,28 @@ public class Person {
 	private String username;
 	private String userpwd;
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getUserpwd() {
+		return userpwd;
+	}
+
+	public void setUserpwd(String userpwd) {
+		this.userpwd = userpwd;
+	}
+
 }
